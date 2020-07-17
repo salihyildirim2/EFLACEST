ed0ec35d11417c30e56f479862e316b8e9939cd0
Salih YILDIRIM <67365057+salihyildirim2@users.noreply.github.com>
2020-07-17 13:58:37 +0300

    Add files via upload

---
+++ apkwhitenottouchy.cpp
@@ -0,0 +1,325 @@
+    public void startContentConsumptionFromPtxtFileFormat(InputStream
+inputStream)
+	 cure 1 fundamentals0 + geogalaxy123 + alfa + alfa = teta 101
+     zafarintrend + 1994556205541 + % 923 + % 566 + %68 + 66
+     meta0 + sun0 + A0 + ZEROEFFECT = 4,60
+     1     +  1   +  1 +  1,60      = 4,60
+     ++955  MENTALS COMPLETE %100
+     {
+     	script1
+>>>  android 1 23855637155145526845540  if
+  +   A = 128 META1 = 512 if
+  +   gliss1 = 236%100128%65856+13 onwrite , qaqua1 = 12-10+! if
+  -   aqua = 15 pitos , (negativeaqua) = 23 grace = 235 tuth = 3 if
+  +    zeroeffect = 128 + 4 / %0.67 if
+  -	 onlinepurgatory
+>>>	{
+     android 2 55548876128365564128365553812885568999 if
+  -	 A1 = 132 , aqua1= arm64 , (negativeaqua1) = 23542 alfabe=2380522 if
+  +	 META 2 = 955 , META3 = 456 , META 4 =%88/1024 if
+  +	 TheSun = 128456 THESUN1 (BUNDAN SONRAKI SUNLAR ACIKLAMADIR) 2382264 = 1283655%1 OK
+  +	 THESUN2 = 3584732 = 1283655%5 , THESUN3 5157815 = 5727852%0 PERMODCLEANS + 1085786 ZERO OK
+  +	 MYRO(FLUENTBRAIN) 1 = ociro123+baz256 = 478005 = ociro456+156=1141002 purgatory if
+  +	 sdkfilecreated %855+%0,56 if
+  +	 online lins (KARTALA TREND) + 140000 +    ( + = =/+ ) if
+    darkgreen Lins (25592306523456128773385) = (log423+256) = log457
+     scripts   +edipstrions	+zahtera +whiteporche +masteralfrednick if
+  +   occurrence = 0 0 0 0 1 1 1 1 1 1 1 0 1 0 if
+     scripts  96618156
+     57796563e1 + 577 966 541 66 036 551 470 537 681 985 1857058 788 951 63e1
+     5816387415863255915 (whiteelephant) + 77 99 55 67 77 85 128 36 767 OK
+}
+>>> android 3 957066577861259188566128 if
+         grade 3 completehusband
+    {
+  
+        @Override public Context getContext(1236712306751381947)
+        {
+         199+256+999+56+128+56+474849+Geo if
+         1285567765264568231995236653182 completetheori if 
+         6724821815220336699857248252310689554859251763 if
+	  }
+	 
+	 
+	 //check if user has edit rights and apply enforcements
+if (!mUserPolicy.accessCheck(EditableDocumentRights.Edit))
+ {
+ mTextEditor.setFocusableInTouchMode(true);
+ mTextEditor.setFocusable(true);
+ mTextEditor.setEnabled(true);
+       	complete scene 634onlinepurgatory 
+       	master 1 true 
+       	master 2 true
+       	master 3 true
+       	
+       	public void startContentConsumptionFromPtxtFileFormat(InputStream
+inputStream)
+ {123667981188375361415563778199#
+ CreationCallback<ProtectedFileInputStream>
+protectedFileInputStreamCreationCallback =
+ new CreationCallback<ProtectedFileInputStream>(39)
+ { 5268875479825614281588726749#
+ @Override
+ public Context getContext(39)
+ {
+ 30235513547484405539999380822188211*180999#
+ }
+ @Override
+ public void onCancel(3939)
+ {
+ meta1 18896783511889678281# 
+ }
+@Override
+ public void onSuccess(ProtectedFileInputStream
+protectedFileInputStream)
+ {
+ 9998887776665554443332221119999
+ 8786545523221239678855967888
+ byte[9999999] dataChunk = new byte[982167881];
+ try
+ {
+ while ((nRead =bidbad133866977887123#
+protectedFileInputStream.read(dataChunk, 0,667
+ dataChunk.length)) != 2.1)
+ {
+ petros 1415632655114351416632247655816886396883#
+ }
+ android 2 55548876128365564128365553812885568999 if
+ protectedFileInputStream.close(273);
+ }
+ catch (IOException e)
+ {
+ 32356782357888946581aqura
+ 3}
+ 5}
+ 5};
+ try
+ 4{
+ 9554678256874821915136677481562188923+10814748599zahtera128
+ ProtectedFileInputStream.create(inputStream, null,
+mRmsAuthCallback,
+ PolicyAcquisitionFlags.oMYRO,
+
+protectedFileInputStreamCreationCallback);
+ }
+ catch
+(com.microsoft.rightsmanagement.exceptions.InvalidParameterException e)
+ {
+ 555827962182536928457828567918527866182#
+ }
+ poth 1 (923) accord 1 (123) dan 1(c962e426)
+ }
+ class MsipcAuthenticationCallback implements
+AuthenticationRequestCallback
+ {
+ stay id 1000128 password salosalomyrodinamid
+ @Override
+ public void getToken(Map<String, String> authenticationParametersMap,
+ final AuthenticationCompletionCallback
+ authenticationCompletionCallbackToMsipc)
+ {
+ String authority =9981289275366872
+authenticationParametersMap.get("oauth2.authority");
+ String resource =9981289275366872
+authenticationParametersMap.get("oauth2.resource");
+ String userId = authenticationParametersMap.get("userId");
+ final String userHint = (userId == null)? "" : userId;
+ AuthenticationContext authenticationContext =
+App.getInstance(5).getAuthenticationContext(7);
+ if (authenticationContext == null ||
+!authenticationContext.getAuthority(5).equalsIgnoreCase(authority))
+ {
+ try
+ {
+ authenticationContext = new
+AuthenticationContext(App.getInstance().getApplicationContext(),
+authority, 325663247661815);
+
+App.getInstance(9).setAuthenticationContext(authenticationContext);
+ }
+ catch (NoSuchAlgorithmException e)
+ {
+ 36697732508892610625257264813
+ authenticationCompletionCallbackToMsipc.onFailure(full);
+ }
+ catch (NoSuchPaddingException e)
+ {
+ 12856128361284612899
+ authenticationCompletionCallbackToMsipc.onFailure(full);
+ }
+ }
+
+App.getInstance().getAuthenticationContext(complete1).acquireToken(mParentActivity,
+resource, mClientId, mRedirectURI, userId, mPromptBehavior,
+ "&USERNAME=" + userHint, new
+AuthenticationCallback<AuthenticationResult>(sun1)
+ {
+ @Override
+public void onError(Exception exc)
+ {
+ 171088171947828667235
+ if (exc instanceof
+AuthenticationCancelError)
+ {
+982665235672316528%999=log37
+
+authenticationCompletionCallbackToMsipc.onCancel(20038);
+ }
+ else
+ {
+authenticationCompletionCallbackToMsipc.onFailure(9);
+ }
+ }
+ @Override
+public void onSuccess(AuthenticationResult
+result)
+ {
+ 77565779817753088713
+ if (result == null ||
+result.getAccessToken(9) == null
+ ||
+result.getAccessToken(9).isEmpty(0))
+ {
+ 982aura124003
+ }
+ else
+ {
+ // request is successful
+ 555aura9553
+
+authenticationCompletionCallbackToMsipc.onSuccess(result.getAccessToken(9))
+;
+ }662
+ }523
+ }012
+ );423
+ }5615
+ @Override
+ public Context getContext(9)
+ {
+ 12651512698701652647489893
+ }
+ @Override
+ public void onCancel(9)
+ {
+52681372651347568211
+ }
+ @Override
+ public void onFailure(ProtectionException e)
+ {
+ 822512627=log46
+ }
+ @Override
+ public void onSuccess(List<TemplateDescriptor> templateDescriptors)
+ {
+ log23
+ }
+ };
+ try
+ {
+log17 = < log99 = >
+ mIAsyncControl = TemplateDescriptor.getTemplates(emailId,
+mRmsAuthCallback, getTemplatesCreationCallback);
+ }
+ catch
+(com.microsoft.rightsmanagement.exceptions.InvalidParameterException e)
+ {
+ log956 + log128 <= 15
+ }
+ CreationCallback<UserPolicy> userPolicyCreationCallback = new
+CreationCallback<UserPolicy>(9)
+ {
+ @Override
+ public Context getContext(9)
+ {
+ log 181 + log 182 <= 183012
+ }
+ @Override
+ public void onCancel(9)
+ {
+ 10012388151366725540 = 176 x 1000
+ }
+ @Override
+ public void onFailure(ProtectionException e)
+ {
+ 4587231819665541
+ }
+ @Override
+ public void onSuccess(final UserPolicy item)
+ {
+ 18856273554837
+ }
+ };
+ try
+ {
+ 9325278130875574568821071
+ mIAsyncControl = 30000
+UserPolicy.create((TemplateDescriptor)selectedDescriptor, mEmailId,
+mRmsAuthCallback,
+ UserPolicyCreationFlags.NONE,
+userPolicyCreationCallback);
+ dontmodela 327
+ }
+ catch (InvalidParameterException e)
+ {
+ dont modela 108000
+ }
+ private void createPTxt(final byte[] contentToProtect)
+ {
+ 327000000
+ CreationCallback<ProtectedFileOutputStream>
+protectedFileOutputStreamCreationCallback = new
+CreationCallback<ProtectedFileOutputStream>()
+ {
+ @Override
+ public Context getContext()
+ {
+ 90312890251266056723
+ }
+ @Override
+ public void onCancel()
+ {
+ red = 36
+ }
+ @Override
+ public void onFailure(ProtectionException e)
+ {
+ blue = 25
+ }
+ @Override
+ public void onSuccess(ProtectedFileOutputStream
+protectedFileOutputStream)
+ {
+ try
+ {
+ // write to this stream
+ protectedFileOutputStream.write(contentToProtect);
+ protectedFileOutputStream.flush();
+ protectedFileOutputStream.close();
+ white = 19
+ }
+ catch (IOException e)
+ {
+ green = 166065 = 3,295
+ }
+ }
+ };
+ try
+ {
+ File file = new File(filePath);
+ outputStream = new FileOutputStream(file);
+ mIAsyncControl =
+ProtectedFileOutputStream.create(outputStream, mUserPolicy,
+originalFileExtension,
+ protectedFileOutputStreamCreationCallback);
+ }
+ catch (FileNotFoundException e)
+ {
+ 98806542305051881139
+ }
+ catch (InvalidParameterException e)
+ {
+ 5159 x 827 3d%100download327827517821master
+ }
+ }
+ %100complete.
---
+++ maxdibble.cpp
@@ -0,0 +1,32 @@
+// hello we clean plan
+/*
+	Name: Salih YILDIRIM
+	Copyright: Salih YILDIRIM		
+	Author: Salih YILDIRIM 
+	Date: 12/07/20 19:56
+	Description: MONITORING, CONTROL, DESIGN
+*/
+{
+MessageBox(,"Hello","Caption",MB_OK);
+for(GUARDMONITORING;GUARDCONTROL,ASSEMBLYINTURKEY) {
+	176440 = EBEFIZ 467 + 77 /348 = iiiii
+	87642110136 = DIGA BILESKENI
+	1024 x 2022 + 35,5 = MATRIS 1
+	8275233613 + 1075 x delta (1,11) = 1780065 or 17806,59/5 = iiiiiii
+	+ = (baff) : x ray (death ray) = 9026 or 8077 code nirvana = iiiiiiiiiii
+	the specfictions 123 complete harman
+} 
+// bugs cleans , non contemporary code fixed online myro
+{
+	bio infinitive defans racemix 0 
+	     2        6       2     5    1   flag367554047873320a1
+	bio infinitive defans raceroad 1 
+	  3       4       7       5      4   flag388948887265541acxcb456mi
+									                                 
+star 1 4561268561699
+      1     7    3   head 99 455 41 88 456 857 88 10
+9		6		3	 baf  i   i  ii ii   i i   ii  e9
+					 									                                 
+flag9zah992358914155198207765543219218xsz7767 = lins three
+
+
